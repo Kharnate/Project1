cfc5e29da79ffc73cdd27285c73d946eba8fd9ed
Kharnate <42840476+Kharnate@users.noreply.github.com>
2019-05-17 17:42:45 -0400

    Comment added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    //Week 1: Procedural Project 1: repl.it
     std::cout << "Production Line Tracker" << std::endl;
     std::cout << ""<< std::endl;
     std::cout << "1. Add Employee Account" << std::endl;
@@ -10,6 +11,7 @@ int main() {
     std::cout << "3. Add Movie Player" << std::endl;
     std::cout << "4. Display Production Statistics" << std::endl;
     std::cout <<"5. Exit";
+    //Week 1 end
 
     return 0;
 }
