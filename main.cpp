f6419b8db0aa746de38eb702f808ee2566a612a3
Kharnate <42840476+Kharnate@users.noreply.github.com>
2019-05-16 21:13:47 -0400

    repl.it 1

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main() {
     std::cout << "2. Add Music Player" << std::endl;
     std::cout << "3. Add Movie Player" << std::endl;
     std::cout << "4. Display Production Statistics" << std::endl;
-
     std::cout <<"5. Exit";
 
     return 0;
