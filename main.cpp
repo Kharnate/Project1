ab5fd21740f2ce3b9805e247943cbc20256cef1f
Kharnate <42840476+Kharnate@users.noreply.github.com>
2019-05-16 21:11:46 -0400

    repl.it 1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    int h;
-    std::cout << "Value of h is: ";
-    std::cin >> h;
+
+    std::cout << "Production Line Tracker" << std::endl;
+    std::cout << ""<< std::endl;
+    std::cout << "1. Add Employee Account" << std::endl;
+    std::cout << "2. Add Music Player" << std::endl;
+    std::cout << "3. Add Movie Player" << std::endl;
+    std::cout << "4. Display Production Statistics" << std::endl;
+    std::cout <<"5. Exit";
+
     return 0;
 }
