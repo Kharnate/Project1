1b8bc0d69b714852f638dcbb46212d3cf81f0960
Kharnate <42840476+Kharnate@users.noreply.github.com>
2019-05-25 13:46:17 -0400

    Merge branch 'master' of C:\Users\kharn\CLionProjects\Project1 with conflicts.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,67 @@
 // ctrl + alt + l for style
-
-/**@file main.cpp
- * @brief Entire media player production program (for now)
- *  A demonstration of functions and files.
- *
- * @author Karan patel
- * @bug
-
- */
-
-
 #include <iostream>
 #include <fstream> // Stream class to both read and write from/to files.
 #include <string>
 
 //Prototype
+void sayHello();
+
+void printSum(int, int);
+
+int calculateSum(int, int);
+
 int showMenu();
-
-void getChoice();
-
-bool continueProgram = true;
 
 int main() {
 
-
-    //Week 1: Procedural Project 1: repl.it
     std::cout << "Production Line Tracker" << std::endl << std::endl;
-    showMenu();
+    //Week 1: Procedural Project 1: repl.it
+    std::cout << "1. Produce Items" << std::endl;
+    std::cout << "2. Add Employee Account" << std::endl;
+    std::cout << "3. Add Music Player" << std::endl;
+    std::cout << "4. Add Movie Player" << std::endl;
+    std::cout << "5. Display Production Statistics" << std::endl;
+    std::cout << "6. Exit" << std::endl;
     //Week 1 end
 
     //Week 2: Procedural Project 2a
-    std::cout << "Production Line Tracker" << std::endl;
-    std::cout << "" << std::endl;
+     int n;
+     std::cout << "Enter a Number: ";
+     std::cin >> n;
 
-    do {
-        showMenu();
-        getChoice();
-    } while (continueProgram);
+     if (n==6) {
+         showMenu();
+     }
+     while (n>0 && n<6) {
+         std::cout << "Produce Items Stub" << std::endl;
+         std::cout << "Enter a Number: ";
+         std::cin >> n;
+         if(n == 6) {
+             showMenu();
+         }
+     }
+     while (n >6){
+         std::cout << "Not a valid Selection" << std::endl;
+         std::cout << "Enter a Number: ";
+         std::cin >> n;
+         if(n == 6) {
+             showMenu();
+         }
+     }
 
-
+    //Class Activity 05/20: Functions
+    sayHello();
+    int num1 = 2;
+    int num2 = 3;
+    printSum(num2, num2); //Sending values to printSum
+    std::cout << "num1 in main is " << num1 << std::endl;
+    int sum = calculateSum(num1, num2);
+    std::cout << "The sum is " << sum << std::endl;
 
     // Files
     std::ofstream myOutputFile;
     myOutputFile.open("example.txt");
-    myOutputFile << "\n\nWriting this to a file.\n";
+    myOutputFile << "Writing this to a file.\n";
     myOutputFile.close(); //Closes the file
     /*
      * This code creates a filled called example.txt
@@ -67,47 +85,33 @@ int main() {
     return 0;
 }
 
-
-int showMenu() {
-
-    std::cout << "1. Produce Items" << std::endl;
-    std::cout << "2. Add Employee Account" << std::endl;
-    std::cout << "3. Add Music Player" << std::endl;
-    std::cout << "4. Add Movie Player" << std::endl;
-    std::cout << "5. Display Production Statistics" << std::endl;
-    std::cout << "6. Exit" << std::endl;
-
-    std::cout << "" << std::endl;
-
-    return 0;
+void sayHello() {
+    std::cout << "Hello World!" << std::endl;
 }
 
-void getChoice() {
-    int choice;
-    std::cin >> choice;
-    switch (choice) {
-        case 1 :
-            std::cout << "Produce Items Stub" << std::endl; //After selecting 1 from the the main menu - showMenu()
-            break;
-        case 2 :
-            std::cout << "Add Employee Account Stub" << std::endl; //Similar as case 1 to case 5
-            break;
-        case 3 :
-            std::cout << "Add Music Player Stub" << std::endl;
-            break;
-        case 4 :
-            std::cout << "Add Movie Player Stub" << std::endl;
-            break;
-        case 5 :
-            std::cout << "Dispplay Production Statistics Stub" << std::endl;
-            break;
-        case 6:
-            continueProgram = false; //When the user enters 6, the do-while ends nad returns to main menu
-            break;
-        default:
-            std::cout << "Not a valid selection" << std::endl;
+
+void printSum(int num1, int num2) {  //Accepting the value from main
+    num1 = 10;
+    std::cout << "The total in printSum is " << num1 + num2 << std::endl;
+}
+
+int calculateSum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int showMenu () {
+        std::cout << "" << std::endl;
+        std::cout << "1. Produce Items" << std::endl;
+        std::cout << "2. Add Employee Accounts" << std::endl;
+        std::cout << "3. Add Music Player " << std::endl;
+        std::cout << "4. Add Movie Player" << std::endl;
+        std::cout << "5. Display Production Statistics" << std::endl;
+        std::cout << "6. Exit" << std::endl;
+
+        std::cout << "" << std::endl;
+
+        return 0;
     }
 
-}
 
 
